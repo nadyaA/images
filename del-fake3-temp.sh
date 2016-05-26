caa1530f2853dbdf2b26ed4931f250071f70d86c
Dimitar Kerezov <d.kerezov@gmail.com>
2016-05-26 17:17:21 +0300

    Invoke deletion with sudo
    Owner of files is root

--- del-fake3-temp.sh
+++ del-fake3-temp.sh
@@ -6,5 +6,5 @@ set +e
 #remove directories older than 1 day
 mtime="-mtime +0"
 
-find /tmp/fakes3_root/ab-builds -maxdepth 1 -type d $mtime -print -exec rm -rf {} \;
-find /tmp/fakes3_root/ab-builds-results -maxdepth 1 -type d $mtime -print -exec rm -rf {} \;
+sudo find /tmp/fakes3_root/ab-builds -maxdepth 1 -type d $mtime -print -exec rm -rf {} \;
+sudo find /tmp/fakes3_root/ab-builds-results -maxdepth 1 -type d $mtime -print -exec rm -rf {} \;
